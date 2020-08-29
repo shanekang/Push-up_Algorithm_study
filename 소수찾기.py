4f2db4402fe9bb4d0e0b004a3342469ed57d762d
shanekang <shane.kang@outlook.com>
2020-08-29 22:32:00 +0900

    playdata

---
+++ 소수찾기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = [True] * (n+1)
+    
+    sqrt_n = int(n ** 0.5)
+    for i in range(2, sqrt_n+1):
+        if answer[i] == True:
+            for j in range(i+i, n+1, i):
+                answer[j] = False
+    
+    return len([i for i in range(2,n+1) if answer[i] == True])
+
+n = 4
+print(solution(n))
